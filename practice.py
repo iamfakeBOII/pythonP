fc34a592f1b4af9e18890805f1140221505b20c1
iamfakeBOII <rayyantumblr@gmail.com>
2020-10-14 12:44:34 +0300

    practice

---
+++ practice.py
@@ -0,0 +1,30 @@
+"""
+n = int(input("n: "))
+dsum = []
+for i in range(1, n+1, 2):
+	dsum.append(i)
+fsum = sum(dsum)
+print("sum: ", fsum)
+"""
+
+"""
+num = int(input("number: "))
+total = []
+for i in range(1, num + 1):
+	if num % i == 0:
+		total.append(i)
+total.remove(num)
+total.remove(1)
+print("factors are: ", total)
+"""
+
+#p3
+
+
+
+
+
+
+			
+
+
