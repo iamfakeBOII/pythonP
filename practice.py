784f94b4511939269ea174905a293dca9b620146
iamfakeBOII <rayyantumblr@gmail.com>
2020-10-15 12:44:31 +0300

    oct15th

--- practice.py
+++ practice.py
@@ -17,8 +17,63 @@ total.remove(num)
 total.remove(1)
 print("factors are: ", total)
 """
-
+"""
 #p3
+limit = int(input("enter upper limit for perfect number search: "))
+n = 1
+while n <= limit:
+
+    sum = 0
+    divisor = 1
+    while divisor < n:
+        if not n % divisor:
+            sum += divisor
+        divisor = divisor + 1
+    if sum == n:
+        print(n, "is a perfect number")
+    n = n + 1
+"""
+
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2 
+# 1
+"""
+row = int(input("rows: "))
+for i in range(row, 0, -1):
+	for j in range(1, i + 1):
+		print(j, end = " ")
+	print("")
+"""
+
+x = 7
+for i in range(4, 0, -1):
+	x = 7
+	for j in range(i, 0, -1):
+		print(x, end = " ")
+		x -= 2
+	print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
